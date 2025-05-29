6893d5933373d6cbfcef5e09a9a810aed05eb5b5
afoote31 <130381602+afoote31@users.noreply.github.com>
2025-05-29 23:09:34 +0000

    Deploying to gh-pages from @ afoote31/afoote31.github.io@990b0fd68407203c578332a3d896d6f440979d26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "post-nfl-tracking-data-project",
         
           title: "NFL Tracking Data Project",
