98d2835298e49ca388621e98333b30dac77af217
afoote31 <130381602+afoote31@users.noreply.github.com>
2025-05-29 22:36:32 +0000

    Deploying to gh-pages from @ afoote31/afoote31.github.io@39184b0f3e0d71880b557d569851455a241e2d24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,8 +459,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-graduated-wesleyan-with-an-ma-in-computer-science-mortar-board",
+          title: 'Graduated Wesleyan with an MA in computer science! :mortar_board:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
