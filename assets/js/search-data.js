d60e77920eef19fac93839ea6b461721839b3731
afoote31 <130381602+afoote31@users.noreply.github.com>
2025-08-23 19:12:18 +0000

    Deploying to gh-pages from @ afoote31/afoote31.github.io@a95bd574c5d2fd445d4993201ef9bceef648a7e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -446,8 +446,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-graduated-wesleyan-with-an-ma-in-computer-science-mortar-board",
-          title: 'Graduated Wesleyan with an MA in computer science! :mortar_board:',
+            },},{id: "news-graduated-with-an-ma-in-computer-science-mortar-board",
+          title: 'Graduated with an MA in computer science! :mortar_board:',
           description: "",
           section: "News",},{id: "news-paper-accepted-for-an-oral-presentation-at-the-ijcai-2025-workshop-on-xai",
           title: 'Paper accepted for an oral presentation at the IJCAI 2025 Workshop on XAI!...',
